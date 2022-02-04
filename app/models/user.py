91f406ef8a80cfc2946ffb6791896cf4c782cdf0
Peter Shin <petershin731@gmail.com>
2022-02-04 09:29:46 -0800

    changed some user database requirements

--- app/models/user.py
+++ app/models/user.py
@@ -8,6 +8,7 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
+    full_name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
@@ -25,6 +26,7 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
+            'full_name': self.full_name,
             'username': self.username,
             'email': self.email
         }
