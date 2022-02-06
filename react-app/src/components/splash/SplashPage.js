0eb704fa7acb3cb1c81984b5bfdeb32ef6d47c34
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-02-06 18:45:05 -0500

    added footer to splash

--- react-app/src/components/splash/SplashPage.js
+++ react-app/src/components/splash/SplashPage.js
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 import LoginForm from "../auth/LoginForm";
+import SplashFooter from "./SplashFooter";
 import "./SplashPage.css"
 import SplashPhone from "./SplashPhone";
 import { login } from "../../store/session";
@@ -14,35 +15,38 @@ import { login } from "../../store/session";
 function SplashPage() {
     const dispatch = useDispatch()
     const handleDemoLogin = () => {
-        const email='demo@aa.io'
-        const password='password'
+        const email = 'demo@aa.io'
+        const password = 'password'
         dispatch(login(email, password));
     }
 
     return (
-        <main className="splash-main">
-            <SplashPhone />
-            <div className="login-signup-container">
-            <div className="splash-login-container">
-                <div className="splash-login-heading">
-                    <h1>Inspogram</h1>
-                </div>
-                    <div className="splash-login-form-container">
-                        <LoginForm />
-                        <div className="demo-container">
-                            <span className="or-span">OR</span>
-                            <button onClick={handleDemoLogin} className="demo-button">Demo login</button>
+        <>
+            <main className="splash-main">
+                <SplashPhone />
+                <div className="login-signup-container">
+                    <div className="splash-login-container">
+                        <div className="splash-login-heading">
+                            <h1>Inspogram</h1>
+                        </div>
+                        <div className="splash-login-form-container">
+                            <LoginForm />
+                            <div className="demo-container">
+                                <span className="or-span">OR</span>
+                                <button onClick={handleDemoLogin} className="demo-button">Demo login</button>
+                            </div>
+                        </div>
+                        <div>
+
                         </div>
                     </div>
-                    <div>
-
+                    <div className="signup-link-container">
+                        <p>Don't have an account? <NavLink className='signup-link' to="/sign-up">Sign up</NavLink></p>
                     </div>
-            </div>
-                <div className="signup-link-container">
-                    <p>Don't have an account? <NavLink className='signup-link' to="/signup">Sign up</NavLink></p>
                 </div>
-            </div>
-        </main>
+            </main>
+            <SplashFooter />
+        </>
 
     )
 }
