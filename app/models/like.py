27bb9e58e868a76b92dc0da3187900af46f5ad41
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-02-07 23:04:43 -0500

    added user key to like dict method

--- app/models/like.py
+++ app/models/like.py
@@ -1,5 +1,5 @@
 from .db import db
-from datetime import datetime 
+from datetime import datetime
 
 class Like(db.Model):
     __tablename__ = "likes"
@@ -10,17 +10,19 @@ class Like(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'is_liked': self.is_liked,
-            'created_at': self.created_at,
-            'user_id': self.user_id,
-            'post_id': self.post_id
-        }
 
      # 1 like belongs to 1 user
     user = db.relationship('User', back_populates='likes')
 
     # 1 like belongs to 1 post
     post = db.relationship('Post', back_populates='likes')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'is_liked': self.is_liked,
+            'created_at': self.created_at,
+            'user_id': self.user_id,
+            'post_id': self.post_id,
+            'user': self.user.to_dict(),
+        }
