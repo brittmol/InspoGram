8d700bc6d4eabda91fb7897b5f69cfa9122e7cad
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-02-10 10:31:00 -0500

    disabled button for empty comment

--- react-app/src/components/Comment/AddCommentForm.js
+++ react-app/src/components/Comment/AddCommentForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; // useEffect,
+import React, { useEffect, useState } from 'react'; // useEffect,
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, useHistory } from "react-router-dom";
 import { createComment } from '../../store/post';
@@ -10,11 +10,21 @@ function AddCommentForm({id, flag}){
     const sessionUser = useSelector((state) => state.session.user);
 
     const [comment, setComment] = useState("");
+    const [disabled, setDisabled] = useState(true)
     const history = useHistory();
     //const [errors, setErrors] = useState([])
     console.log('in component ***********************(')
+    useEffect(() => {
+        if (comment.length > 0) {
+            setDisabled(false)
+        } else {
+            setDisabled(true)
+        }
+    }, [disabled, comment])
 
     if (!sessionUser) return <Redirect to="/" />;
+
+
 
     const handleUserSubmit = async (e) => {
         e.preventDefault()
@@ -31,6 +41,7 @@ function AddCommentForm({id, flag}){
         dispatch(createUserComment(payload))
         // history.push('/feed')
     }
+
 
 
 
@@ -58,7 +69,7 @@ function AddCommentForm({id, flag}){
                 value={comment}
                 onChange={(e) => setComment(e.target.value)}
             />
-            <button type="submit">
+            <button disabled={disabled} type="submit">
                 Post
             </button>
         </form>
