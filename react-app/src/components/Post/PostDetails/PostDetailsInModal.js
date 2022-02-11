5521b342a5cbdb61e6fb1707944ce806625122ba
Brittany Moliver <britt.moliver@gmail.com>
2022-02-11 14:02:35 -0700

    britts css added

--- react-app/src/components/Post/PostDetails/PostDetailsInModal.js
+++ react-app/src/components/Post/PostDetails/PostDetailsInModal.js
@@ -4,15 +4,16 @@ import { NavLink } from "react-router-dom";
 
 import DisplayAllComments from "../../Comment/DIsplayAllComments";
 import { deleteUserPost, getUserPosts } from "../../../store/userPosts";
+import MenuModal from "./ProfileMenuModal";
 import { getASinglePost } from "../../../store/post";
 import AddCommentForm from "../../Comment/AddCommentForm";
 import PostProfileLikes from "./PostProfileLikes";
-import MenuModal from "./ProfileMenuModal";
 // import EditPostModal from "../EditPost";
 // import SinglePost from "../../Feed/SinglePost";
 // import DisplayComment from "../../Comment/DisplayComments";
 
 import "./PostDetails.css";
+import cat from "../../../images/cat.jpg";
 
 function PostDetails({ post, onClose }) {
   //const singlePost = useSelector(state => state.userPostsReducer[post.id])
@@ -35,26 +36,72 @@ function PostDetails({ post, onClose }) {
         <img className="profile-modal-img" src={post?.photos[0]?.photo}></img>
       </div>
       <div className="profile-modal-data-container">
-        <div className="profile-modal-username">
-          <NavLink onClick={onClose} to={`/users/${post?.users?.id}`}>
-            {post?.users?.username}
-          </NavLink>
-
-          <MenuModal post={post} />
+        <div className="profile-modal-top">
+          <div className="profile-modal-username singular-comment-container">
+            <NavLink to={`/users/${post?.users?.id}`}>
+              <img className="profile-pic-comment user" src={cat} alt="cat" />
+            </NavLink>
+            <NavLink
+              to={`/users/${post?.users?.id}`}
+              className="comment-username"
+            >
+              {post?.users?.username}
+            </NavLink>
+            <MenuModal post={post} />
+          </div>
+          <div className="profile-modal-caption singular-comment-container">
+            <NavLink to={`/users/${post?.users?.id}`} activeClassName="active">
+              <img className="profile-pic-comment user" src={cat} alt="cat" />
+            </NavLink>
+            <NavLink
+              to={`/users/${post?.users?.id}`}
+              className="comment-username"
+            >
+              {post?.users?.username}
+            </NavLink>
+            <p className="actual-comment">{post.caption}</p>
+          </div>
         </div>
-        <div className="profile-modal-caption">{post?.caption}</div>
-        <div>
+        <div className="profile-modal-comments">
           <DisplayAllComments comments={post.comments} />
         </div>
-        <div>
-          <PostProfileLikes post={post} />
-        </div>
-        <div>
-          <AddCommentForm flag={true} id={post.id} />
+        <div className="profile-modal-bottom">
+          <div>
+            <PostProfileLikes post={post} />
+          </div>
+          <div>
+            <AddCommentForm flag={true} id={post.id} />
+          </div>
         </div>
       </div>
     </>
   );
+  // return (
+  //   <>
+  //     <div className="profile-modal-img-container">
+  //       <img className="profile-modal-img" src={post?.photos[0]?.photo}></img>
+  //     </div>
+  //     <div className="profile-modal-data-container">
+  //       <div className="profile-modal-username">
+  //         <NavLink onClick={onClose} to={`/users/${post?.users?.id}`}>
+  //           {post?.users?.username}
+  //         </NavLink>
+
+  //         <MenuModal post={post} />
+  //       </div>
+  //       <div className="profile-modal-caption">{post?.caption}</div>
+  //       <div>
+  //         <DisplayAllComments comments={post.comments} />
+  //       </div>
+  //       <div>
+  //         <PostProfileLikes post={post} />
+  //       </div>
+  //       <div>
+  //         <AddCommentForm flag={true} id={post.id} />
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 }
 
 export default PostDetails;
