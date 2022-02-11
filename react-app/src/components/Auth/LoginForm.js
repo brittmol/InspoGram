95a3159425da20a692c39f440cb16b7c67d0f66c
Brittany Moliver <britt.moliver@gmail.com>
2022-02-11 14:04:57 -0700

    corrected login redirection

--- react-app/src/components/Auth/LoginForm.js
+++ react-app/src/components/Auth/LoginForm.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import { login } from '../../store/session';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { login } from "../../store/session";
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [disableButton, setDisableButton] = useState(true);
-  const user = useSelector(state => state.session.user);
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (email.includes('@') && password.length >= 6) {
-      setDisableButton(false)
+    if (email.includes("@") && password.length >= 6) {
+      setDisableButton(false);
     } else {
-      setDisableButton(true)
+      setDisableButton(true);
     }
-  }, [disableButton, email, password])
+  }, [disableButton, email, password]);
 
   const onLogin = async (e) => {
     e.preventDefault();
@@ -36,40 +36,46 @@ const LoginForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/feed" />;
   }
 
   return (
-    <form className='splash-login-form' onSubmit={onLogin}>
-      <div className='login-error-container'>
+    <form className="splash-login-form" onSubmit={onLogin}>
+      <div className="login-error-container">
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div className='login-input-container'>
-        <div className='input-container'>
+      <div className="login-input-container">
+        <div className="input-container">
           <input
-            className='form-input'
-            name='email'
-            type='text'
-            placeholder='Email'
+            className="form-input"
+            name="email"
+            type="text"
+            placeholder="Email"
             value={email}
             onChange={updateEmail}
           />
-          <label className='form-label' htmlFor='email'>Email</label>
+          <label className="form-label" htmlFor="email">
+            Email
+          </label>
         </div>
-        <div className='input-container'>
+        <div className="input-container">
           <input
-            className='form-input'
-            name='password'
-            type='password'
-            placeholder='Password'
+            className="form-input"
+            name="password"
+            type="password"
+            placeholder="Password"
             value={password}
             onChange={updatePassword}
           />
-          <label className='form-label' htmlFor='password'>Password</label>
+          <label className="form-label" htmlFor="password">
+            Password
+          </label>
         </div>
-          <button className='form-button' disabled={disableButton} type='submit'>Login</button>
+        <button className="form-button" disabled={disableButton} type="submit">
+          Login
+        </button>
       </div>
     </form>
   );
