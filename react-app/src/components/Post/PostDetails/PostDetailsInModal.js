e47569b7ec74d3579cba5750259e97c3772aad94
Brittany Moliver <britt.moliver@gmail.com>
2022-02-10 18:18:43 -0700

    some css for postdetails modal

--- react-app/src/components/Post/PostDetails/PostDetailsInModal.js
+++ react-app/src/components/Post/PostDetails/PostDetailsInModal.js
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { deleteUserPost } from "../../../store/userPosts";
 import EditPostModal from "../EditPost";
-import './PostDetails.css'
+import "./PostDetails.css";
 import MenuModal from "./ProfileMenuModal";
 // import DisplayComment from "../../Comment/DisplayComments";
 import AddCommentForm from "../../Comment/AddCommentForm";
@@ -10,45 +10,46 @@ import DisplayAllComments from "../../Comment/DIsplayAllComments";
 import SinglePost from "../../Feed/SinglePost";
 import PostProfileLikes from "./PostProfileLikes";
 
-
 function PostDetails({ post, onClose }) {
-    //const singlePost = useSelector(state => state.userPostsReducer[post.id])
-    const dispatch = useDispatch()
+  //const singlePost = useSelector(state => state.userPostsReducer[post.id])
+  const dispatch = useDispatch();
 
-    const handleDelete = (e) => {
-        const id = e.target.id
-        dispatch(deleteUserPost(id))
-        onClose()
-    }
+  const handleDelete = (e) => {
+    const id = e.target.id;
+    dispatch(deleteUserPost(id));
+    onClose();
+  };
 
+  return (
+    <>
+      <div className="profile-modal-img-container">
+        <img className="profile-modal-img" src={post?.photos[0]?.photo}></img>
+      </div>
+      <div className="profile-modal-data-container">
+        <div className="profile-modal-top">
+          <div className="profile-modal-username">
+            <NavLink onClick={onClose} to={`/users/${post?.users?.id}`}>
+              {post?.users?.username}
+            </NavLink>
 
-    return (
-        <>
-            <div className="profile-modal-img-container">
-                <img className="profile-modal-img" src={post?.photos[0]?.photo} ></img>
-            </div>
-            <div className="profile-modal-data-container">
-                <div className="profile-modal-username">
-                    <NavLink onClick={onClose} to={`/users/${post?.users?.id}`}>{post?.users?.username}</NavLink>
-
-                    <MenuModal post={post} />
-                </div>
-                <div className="profile-modal-caption" >
-                    {post?.caption}
-                </div>
-                <div>
-                    <DisplayAllComments comments={post.comments}/>
-                </div>
-                <div>
-                    <PostProfileLikes post={post}/>
-                </div>
-                <div>
-                    <AddCommentForm flag={true} id={post.id}/>
-                </div>
-
-            </div>
-        </>
-    )
+            <MenuModal post={post} />
+          </div>
+          <div className="profile-modal-caption">{post?.caption}</div>
+          <div className="profile-modal-comments">
+            <DisplayAllComments comments={post.comments} />
+          </div>
+        </div>
+        <div className="profile-modal-bottom">
+          <div>
+            <PostProfileLikes post={post} />
+          </div>
+          <div>
+            <AddCommentForm flag={true} id={post.id} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default PostDetails;
