5685cc4c5cad52c1620807056056e312b9e7341c
Megan McKenna <meganemckenna1@gmail.com>
2022-02-11 12:37:18 -0600

    added nav styling

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -10,25 +10,34 @@ import cat from '../../images/cat.jpg'
 
 
 
-const NavBar = ({user}) => {
+const NavBar = ({ user }) => {
   return (
+
     <nav>
-      <div className='left-bar'>
-        <NavLink to='/feed' exact={true} activeClassName='active'>
-          <img className='logo' src={logo} alt='logo' />
-          {/* <p className='logo'>Inspogram</p> */}
-        </NavLink>
-      </div>
-      <div className='right-bar'>
-        <NavLink to='/feed' exact={true} activeClassName='active'>
-          <i className="fas fa-home"></i>
-          {/* <i className="fa-solid fa-house"></i> */}
-        </NavLink>
-        <CreatePostModal />
-        <NavLink to={`/users/${user.id}`} activeClassName='active'>
-          <img className='profile-pic-nav' src={cat} alt='cat' />
-        </NavLink>
-        <LogoutButton />
+      <div className='nav-container'>
+        <div className='nav-bar'>
+
+          <div className='left-bar'>
+            <NavLink to='/feed' exact={true} activeClassName='active'>
+              <img className='logo' src={logo} alt='logo' />
+              {/* <p className='logo'>Inspogram</p> */}
+            </NavLink>
+          </div>
+
+          <div className='right-bar'>
+            <NavLink to='/feed' exact={true} activeClassName='active'>
+              <i className="fas fa-home"></i>
+              {/* <i className="fa-solid fa-house"></i> */}
+            </NavLink>
+            <CreatePostModal />
+            <NavLink to={`/users/${user.id}`} activeClassName='active'>
+              <img className='profile-pic-nav' src={cat} alt='cat' />
+            </NavLink>
+            <LogoutButton />
+          </div>
+
+        </div>
+
       </div>
     </nav>
   );
