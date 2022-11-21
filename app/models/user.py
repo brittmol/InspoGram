fd53abdef9c0209390a01267e728cd27d69f9648
Brittany Moliver <britt.moliver@gmail.com>
2022-11-21 01:46:51 -0500

    models/user.py instance variable = follows

--- app/models/user.py
+++ app/models/user.py
@@ -10,7 +10,7 @@ follows = db.Table(
     db.Column("followed_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
 )
 if environment == "production":
-    <instance_variable>.schema = SCHEMA
+    follows.schema = SCHEMA
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
